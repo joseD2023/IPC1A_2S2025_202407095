4b8da423bc16e0d58f2a99eaec04559c3c42e9d7
PythonDelarosa <josevictordelarosachico694@gmail.com>
2025-08-02 15:47:55 -0600

    IPC1 HOLA

---
+++ practica1_ipc1/Practica1_IPC1.java
@@ -0,0 +1,13 @@
+
+package practica1_ipc1;
+
+public class Practica1_IPC1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}

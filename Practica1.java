374b8a80f5488c3d6f04e7239e0b2b00ecb28048
PythonDelarosa <josevictordelarosachico694@gmail.com>
2025-08-12 15:12:21 -0600

    Prime Commit practica 1

---
+++ Practica1.java
@@ -0,0 +1,87 @@
+import java.util.Scanner;
+
+public class Practica1 {
+    //DEFINIMOS LA ENTRADA A CONSOLA COMO VARIABLE GLOBAL
+    static Scanner sc = new Scanner(System.in);
+
+    /*-------------------------------------------------------------------------*/
+
+    public static void main(String[] args){
+        MenuGestionesPersonajes();
+
+    }
+
+    public static void MenuGestionesPersonajes(){
+        while(true){
+            System.out.println("+--- MENU DE GESTION DE PERSONAJES ---+");
+            System.out.println("1. Agregar Personajes ");
+            System.out.println("2. Modificar Personaje");
+            System.out.println("3. Eliminar Personaje ");
+            System.out.println("4. Ver Datos de un Personaje ");
+            System.out.println("5. Ver listado de Personajes ");
+            System.out.println("6. Realizar pelea entre personajes");
+            System.out.println("7. Ver Historial de peleas ");
+            System.out.println("8. Ver datos de Estudiante ");
+            System.out.println("9. Salir ");
+            System.out.println(" Elegir una Opcion: ");
+
+            int opc = sc.nextInt();
+            sc.nextLine(); // Borrar La entrada
+
+            switch (opc){
+                case 1:
+                    System.out.println("[INGRESE UN NOMBRE: ]");
+                    String name = sc.nextLine();
+                    sc.nextLine();
+                    System.out.println("[INGRESE UN ARMA: ]");
+                    String arm = sc.nextLine();
+                    GestorPersonajes.AgregarPesonajes(name, arm);
+                    break;
+                case 2:
+                    System.out.println("[INGRESE UN NOMBRE: ]");
+                    String name3 = sc.nextLine();
+                    GestorPersonajes.ModificarPersonajes(name3);
+
+                    break;
+                case 3:
+                    sc.nextLine();
+                    System.out.println("[INGRESE UN NOMBRE]");
+                    String name4 = sc.nextLine();
+                    GestorPersonajes.eliminarPersonajes(name4);
+
+                    break;
+                case 4:
+                    System.out.println("[INGRESE UN NOMBRE]");
+                    String name2 = sc.nextLine();
+                    GestorPersonajes.verDatosDeUnPersonaje(name2);
+                    break;
+                case 5:
+                    GestorPersonajes.verListadoPersonajes();
+                    break;
+
+                case 6:
+                    sc.nextLine();
+                    System.out.println("[INGRESE UN NOMBRE PARA P1]");
+                    String p1 = sc.nextLine();
+                    System.out.println("[INGRESE UN NOMBRE PARA P1]");
+                    String p2 = sc.nextLine();
+                    GestorPersonajes.peleaPersoanejes(p1,p2);
+                    break;
+                case 7:
+                    break;
+                case 8:
+                    break;
+                case 9:
+                    System.out.println("Gracias por Usar Nuestro Programa de IPC1");
+                    break;
+                default:
+                    System.out.println("OPCION INVALIDA !!");
+                    break;
+
+            }
+
+
+
+        }
+    }
+}
